e5a7a5b15c9ec5cb11b1ca108e3d71fd7ddeb037
Nikita Kivalin <kivalinnik@mail.ru>
2024-11-29 23:20:37 +0300

    pull

--- src/main/java/com/quackaboutit/equipmentapp/analytics/controller/AnalyticsController.java
+++ src/main/java/com/quackaboutit/equipmentapp/analytics/controller/AnalyticsController.java
@@ -1,9 +1,7 @@
 package com.quackaboutit.equipmentapp.analytics.controller;
 
 import com.quackaboutit.equipmentapp.analytics.service.ExcelTableService;
-import jakarta.annotation.Resource;
 import lombok.RequiredArgsConstructor;
-import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
