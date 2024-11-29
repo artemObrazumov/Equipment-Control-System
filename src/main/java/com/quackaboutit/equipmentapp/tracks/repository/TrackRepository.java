dce47376ddf787aae4805bde93542fae9ab9040d
artemObrazumov <a.obraz60022006@gmail.com>
2024-11-29 14:03:32 +0300

    track fixes

--- src/main/java/com/quackaboutit/equipmentapp/tracks/repository/TrackRepository.java
+++ src/main/java/com/quackaboutit/equipmentapp/tracks/repository/TrackRepository.java
@@ -1,5 +1,6 @@
 package com.quackaboutit.equipmentapp.tracks.repository;
 
+import com.quackaboutit.equipmentapp.tracks.entity.ArrivalPoint;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -20,4 +21,7 @@ public interface TrackRepository extends JpaRepository<Track, Long> {
     @Modifying
     @Query("UPDATE Track t SET t.driver = :driver WHERE t.id = :id")
     void updateDriver(@PathParam("driver") String driver, @PathParam("id") Long id);
+
+    @Query("SELECT t From Track t WHERE :point MEMBER OF t.arrivalPoint")
+    Track findByArrivalPoint(@PathParam("point") ArrivalPoint point);
 }
